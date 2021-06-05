846cbb3fb290f1663fb2b7eb7ea52f54e8724ee8
Mekyle Logen Naidoo <mekylenaidoo@yahoo.com>
2021-06-05 09:11:34 +0200

    Added better instructions on usage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,7 @@
 <template>
-
   <div>
       <button class="button" v-on:click="greet">Connect wallet</button>
-      <p >{{ name }}: {{ address }}</p>
+      <p >{{ name }}: {{ address }}</p> <!-- This takes in the account name and the account address and displays it to the user after there has been a connection to PolkadotJS -->
   </div>
 </template>
 <style>
@@ -34,11 +33,11 @@ export default {
     async greet() {
       // returns an array of all the injected sources
       //   (this needs to be called first, before other requests)
-      await web3Enable("Salmon");
+      await web3Enable("Starter"); // You can name this whatever your dapp is called. Just change the text in web3Enable.
       await web3Accounts()
       .then((res) => {
         
-        return ( this.name = res[0].meta.name, this.address = res[0].address )
+        return ( this.name = res[0].meta.name, this.address = res[0].address ) // This returns the address at the zero position on the array. If you remove the array, it'll show you all available addresses.
       })
     },
   },
